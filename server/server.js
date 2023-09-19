2655eda84cb08eab228c336b1fd73a55a94d3a04
Shohag <tashohag1@gmail.com>
2023-09-19 20:14:25 +0300

    confirm
    yes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 require('dotenv').config()
 const app=require('./app')
-const colors=require('colors')
-// const Databage = require('./config/DB')
 const PORT=process.env.PORT
 
 app.listen(PORT,async()=>{
-    // console.log(colors.bgGray(`server running at http://localhost:${PORT}`));
-// await Databage()
+    console.log(`server running at http://localhost:${PORT}`);
 })
